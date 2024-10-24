3e300a44aa522399ad979bf91712c5adc81fa158
Simone Amighini <simone.amighini@mail.polimi.it>
2024-10-24 17:58:50 +0200

    Add clock enable to REG_PP and update its testbench

--- sim/TB_REG_PP.vhd
+++ sim/TB_REG_PP.vhd
@@ -5,24 +5,26 @@ entity TB_REG_PP is
 end TB_REG_PP;
 
 architecture BHV of TB_REG_PP is
-    constant CLK_PERIOD: time := 1 ns;
+    constant CLK_PERIOD: time := 10 ns;
     constant REG_NUMBER: integer := 8;
     
     component REG_PP is
         port(
             CLK: in std_logic;
+            EN: in std_logic;
             RST: in std_logic;
             D_IN: in std_logic_vector(REG_NUMBER-1 downto 0);
             D_OUT: out std_logic_vector(REG_NUMBER-1 downto 0)
         );
     end component;
     
-    signal CLK, RST: std_logic;
+    signal CLK, EN, RST: std_logic;
     signal D_IN, D_OUT: std_logic_vector(REG_NUMBER-1 downto 0);
 begin
     UUT: REG_PP
     port map(
         CLK => CLK,
+        EN => EN,
         RST => RST,
         D_IN => D_IN,
         D_OUT => D_OUT
@@ -39,12 +41,16 @@ begin
     
     SIM: process is
     begin
-        wait for CLK_PERIOD * 2.3;
-        
+        EN <= '1';
         RST <= '1';
-        wait for CLK_PERIOD * 5;
+        wait for CLK_PERIOD * 10;
         
         RST <= '0';
+        D_IN <= "10000011";
+        wait for CLK_PERIOD * 3;
+        
+        wait for CLK_PERIOD * 5;
+        
         D_IN <= "10000000";
         wait for CLK_PERIOD * 5;
         
@@ -52,6 +58,11 @@ begin
         wait for CLK_PERIOD * 5;
         
         RST <= '1';
+        wait for CLK_PERIOD * 5;
+        
+        EN <= '0';
+        RST <= '0';
+        D_IN <= "10000000";
         wait;
     end process;
 end BHV;

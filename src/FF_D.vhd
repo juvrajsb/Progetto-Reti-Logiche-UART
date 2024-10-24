dc53db5c7579cdb6cd099b3499521d4995535d0d
Simone Amighini <simone.amighini@mail.polimi.it>
2024-10-24 15:44:11 +0200

    Add clock enable to FF_D and update its testbench

--- src/FF_D.vhd
+++ src/FF_D.vhd
@@ -4,6 +4,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity FF_D is
     port(
         CLK: in std_logic;
+        EN: in std_logic;
         RST: in std_logic;
         D: in std_logic;
         Q: out std_logic
@@ -14,11 +15,11 @@ architecture BHV of FF_D is
 begin
     process(CLK) is
     begin
-        if rising_edge(CLK) then
+        if CLK'event and CLK='1' then
             -- Syncronous reset
             if RST='1' then
                 Q <= '0';
-            else
+            elsif EN='1' then
                 Q <= D;
             end if;
         end if;

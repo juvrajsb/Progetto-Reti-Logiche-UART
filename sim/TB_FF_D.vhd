45d3c6eba22d8bf8c3b14c04e58a1a170d2147ea
Simone Amighini <dev@simoneamighini.it>
2024-10-24 15:44:11 +0200

    Add clock enable to FF_D and update its testbench

--- sim/TB_FF_D.vhd
+++ sim/TB_FF_D.vhd
@@ -10,19 +10,21 @@ architecture BHV of TB_FF_D is
     component FF_D is
         port(
             CLK: in std_logic;
+            EN: in std_logic;
             RST: in std_logic;
             D: in std_logic;
             Q: out std_logic
         );
     end component;
     
-    signal CLK, RST, D, Q: std_logic;
+    signal CLK, EN, RST, D, Q: std_logic;
 begin
     UUT: FF_D
     port map(
-        D => D,
         CLK => CLK,
+        EN => EN,
         RST => RST,
+        D => D,
         Q => Q
     );
     
@@ -37,14 +39,19 @@ begin
     
     SIM: process is
     begin
+        EN <= '1';
+        RST <= '1';
+    
         wait for CLK_PERIOD * 9.4;
-        RST <= '0';
-        
-        wait for CLK_PERIOD * 20;
         D <= '0';
+        RST <= '0';
         
         wait for CLK_PERIOD * 5;
         D <= '1';
+        
+        wait for CLK_PERIOD * 5;
+        EN <= '0';
+        D <= '0';
         
         wait for CLK_PERIOD * 5;
         RST <= '1';

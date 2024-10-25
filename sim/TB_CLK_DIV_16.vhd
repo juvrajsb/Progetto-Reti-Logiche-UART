914ed97b0fe232fb7e9c01c39e7c949770a9ae68
Simone Amighini <simone.amighini@mail.polimi.it>
2024-10-25 15:10:58 +0200

    Improve CLK_DIV_16 and its testbench

--- sim/TB_CLK_DIV_16.vhd
+++ sim/TB_CLK_DIV_16.vhd
@@ -4,34 +4,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity TB_CLK_DIV_16 is
 end TB_CLK_DIV_16;
 
-architecture behavior of TB_CLK_DIV_16 is
+architecture BHV of TB_CLK_DIV_16 is
     component CLK_DIV_16
         port(
-            CLK    : in  std_logic;
+            CLK_X16: in  std_logic;
             RST    : in  std_logic;
-            CLK_DIV  : out std_logic
+            CLK_X1 : out std_logic
         );
     end component;
 
-    constant CLK_PERIOD : time := 10 ns;
+    constant CLK_PERIOD: time := 10 ns;
     
-    signal CLK    : std_logic := '0';
-    signal RST    : std_logic := '0';
-    signal CLK_DIV  : std_logic;
-    
+    signal CLK_X16: std_logic;
+    signal RST    : std_logic;
+    signal CLK_X1 : std_logic;
 begin
     UUT: CLK_DIV_16
         port map (
-            CLK    => CLK,
-            RST    => RST,
-            CLK_DIV  => CLK_DIV
+            CLK_X16 => CLK_X16,
+            RST     => RST,
+            CLK_X1  => CLK_X1
         );
 
     CLK_PROCESS: process
     begin
-        CLK <= '0';
+        CLK_X16 <= '0';
         wait for CLK_PERIOD/2;
-        CLK <= '1';
+        CLK_X16 <= '1';
         wait for CLK_PERIOD/2;
     end process;
 
@@ -39,17 +38,15 @@ begin
     begin
         RST <= '1';
         wait for CLK_PERIOD*10;
-        RST <= '0';
         
+        RST <= '0';
         wait for CLK_PERIOD*100;
         
         RST <= '1';
         wait for CLK_PERIOD*2;
-        RST <= '0';
         
-        wait for CLK_PERIOD*100;
+        RST <= '0';
         
         wait;
     end process;
-
-end behavior;
+end BHV;

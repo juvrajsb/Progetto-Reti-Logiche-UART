61e511add4ee4639ec03ecc1eef057b598b6e05e
Juvraj Singh Banwait <10777524@polimi.it>
2024-11-02 10:42:42 +0100

    Switch to REP_PS with 9 bits

--- src/REG_PS.vhd
+++ src/REG_PS.vhd
@@ -18,7 +18,7 @@ end REG_PS;
 
 architecture RTL of REG_PS is
     -- Q signal keeps track of the registers' state
-    signal Q: std_logic_vector(REG_NUMBER-1 downto 0);
+    signal Q: std_logic_vector(REG_NUMBER downto 0);
 begin
     process(CLK, RST) is
     begin
@@ -27,10 +27,10 @@ begin
         elsif CLK'event and CLK='1' then
             if EN='1' then
                 if LOAD='1' then
-                    Q <= D_IN;
+                    Q <= D_IN & '0'; -- Add an empty bit to avoid loss of D_IN(0) after the first right shift
                 else
                     -- Right shift
-                    Q <= '0' & Q(REG_NUMBER-1 downto 1);
+                    Q <= '0' & Q(REG_NUMBER downto 1);
                 end if;
             end if;
         end if;

f74f743620ca229b6d1fd1dd79f31b021597baca
Simone Amighini <simone.amighini@mail.polimi.it>
2024-11-03 10:48:51 +0100

    Add a complete implementation of TRANSMISSION module with its testbench

--- src/TRANSMITTER.vhd
+++ src/TRANSMITTER.vhd
@@ -4,7 +4,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity TRANSMITTER is
     port(
         CLK: in std_logic;
-        DIN: in std_logic_vector(7 downto 0);
+        EN: in std_logic;
+        RST: in std_logic;
+        D_IN: in std_logic_vector(7 downto 0);
         START: in std_logic;
         CTS: in std_logic;
         LEN: in std_logic;
@@ -22,17 +24,70 @@ architecture RTL of TRANSMITTER is
         );
     end component;
     
+    component REG_PS is
+        generic(
+            REG_NUMBER: integer := 8
+        );
+        
+        port(
+            CLK: in std_logic;
+            EN: in std_logic;
+            RST: in std_logic;
+            D_IN: in std_logic_vector(REG_NUMBER-1 downto 0);
+            LOAD: in std_logic;
+            D_OUT: out std_logic
+        );
+    end component;
+    
+    component TX_FSM is
+    port(
+        CLK: in std_logic;
+        EN: in std_logic;
+        RST: in std_logic;
+        PS_REG_SHIFT_BIT: in std_logic;
+        START: in std_logic;
+        CTS: in std_logic;
+        PS_REG_LOAD: out std_logic;
+        BIT_TO_SEND: out std_logic
+    );
+end component;
     signal PAR_BIT: std_logic;
-    signal TX_DATA: std_logic_vector(7 downto 0);
+    signal PS_REG_DATA: std_logic_vector(7 downto 0);
+    signal PS_REG_SHIFT_BIT: std_logic;
+    signal PS_REG_LOAD: std_logic;
 begin
+    -- INPUT ELABORATION + SELECTION BASED ON LEN AND PARITY
     PARITY_CALC: PAR_7
     port map(
-        DATA => DIN(6 downto 0),
+        DATA => D_IN(6 downto 0),
         ODD_MODE => PARITY,
         PAR_BIT => PAR_BIT
     );
     
-    -- Input selection
-    TX_DATA <= (PAR_BIT & DIN(6 downto 0)) when LEN='0' else
-               DIN;
+    PS_REG_DATA <= (PAR_BIT & D_IN(6 downto 0)) when LEN='0' else
+                   D_IN;
+    
+    -- SAVE ELABORATED INPUT
+    REG: REG_PS
+    port map(
+        CLK => CLK,
+        EN => EN,
+        RST => RST,
+        D_IN => PS_REG_DATA,
+        LOAD => PS_REG_LOAD,
+        D_OUT => PS_REG_SHIFT_BIT
+    );
+    
+    -- MANAGE TRASMISSION USING A FSM
+    FSM: TX_FSM
+    port map(
+        CLK => CLK,
+        EN => EN,
+        RST => RST,
+        PS_REG_SHIFT_BIT => PS_REG_SHIFT_BIT,
+        START => START,
+        CTS => CTS,
+        PS_REG_LOAD => PS_REG_LOAD,
+        BIT_TO_SEND => TX
+    );
 end RTL;
